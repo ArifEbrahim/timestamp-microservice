66a17f8e15ffce34a97c62c0afea657aaffdd26c
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-01-16 16:05:45 +0000

    Server can parse unix date and respond

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/api/:date", (req, res) => {
-    const dt = DateTime.fromISO(req.params.date);
-    const dateResponse = {
-        unix: dt.toMillis(),
-        utc: dt.toFormat('EEE, dd MMM yyyy HH:mm:ss ZZZZ')
+    const userDate = req.params.date;
+    let dateResponse = {};
+    if (userDate.length === 13) {
+        const dt = DateTime.fromMillis(Number(userDate));
+        dateResponse = {
+            unix: Number(userDate),
+            utc: dt.toFormat("EEE, dd MMM yyyy HH:mm:ss ZZZZ"),
+        };
+    } else {
+        const dt = DateTime.fromISO(userDate);
+        dateResponse = {
+            unix: dt.toMillis(),
+            utc: dt.toFormat("EEE, dd MMM yyyy HH:mm:ss ZZZZ"),
+        };
     }
     res.json(dateResponse);
 });
--- tests/server.test.js
+++ tests/server.test.js
@@ -4,13 +4,21 @@ const app = require("../server");
 describe("Get /api/:date", () => {
     it("returns Unix timestamp when date is valid", async () => {
         const response = await request(app).get("/api/2022-01-16");
-        expect(response.body).toHaveProperty('unix')
-        expect(response.body.unix).toBe(1642291200000)
+        expect(response.body).toHaveProperty("unix");
+        expect(response.body.unix).toBe(1642291200000);
     });
 
-    it('returns a utc time when date is valid', async () => {
+    it("returns a utc time when date is valid", async () => {
         const response = await request(app).get("/api/2022-01-16");
-        expect(response.body).toHaveProperty('utc');
-        expect(response.body.utc).toBe('Sun, 16 Jan 2022 00:00:00 GMT')
-    })
+        expect(response.body).toHaveProperty("utc");
+        expect(response.body.utc).toBe("Sun, 16 Jan 2022 00:00:00 GMT");
+    });
+
+    it("returns the correct date when given unix time", async () => {
+        const response = await request(app).get("/api/1451001600000");
+        expect(response.body).toEqual({
+            unix: 1451001600000,
+            utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+        })
+    });
 });
