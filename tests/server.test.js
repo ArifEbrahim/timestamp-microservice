8451baf9dabe1a4888c1700f3cdcc48b286e2dba
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2022-01-16 06:09:36 +0000

    Setup server and basic API testing

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,9 @@
-describe('test suite', () => {
-    it('example assertion', () => {
-        expect(true).toEqual(true)
-    })
-})
+const request = require("supertest");
+const app = require("../server");
+
+describe("App test", () => {
+    it("returns hello world", async () => {
+        const response = await request(app).get("/");
+        expect(response.text).toEqual("Hello World");
+    });
+});
